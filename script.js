0e234e856d64c823d2baf95d2bdc4812cad9be58
TaeSym <shauntesymone@gmail.com>
2024-01-03 19:43:24 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var timeOfDay = [6, "noon", 8, "morning", "evening", 12];
+console.log(timeOfDay[3]);
+
+timeOfDay[3] = "midnight";
+console.log(timeOfDay);
+
+timeOfDay.push("morning");
+console.log(timeOfDay);
+
+timeOfDay.splice(0, 2);
+console.log(timeOfDay);
+
